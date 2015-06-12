356696aa0a6c1536562599d86b78a391a163961a
Heiner León <h.leon12p@gmail.com>
2015-06-12 17:46:35 -0600

    More efficient

--- Morse/Morse.ino
+++ Morse/Morse.ino
@@ -1,167 +1,70 @@
-char letter; //Store a char
-String message = ""; //Message to encode to Morse Code
-int messageLen = 0; //Message's lenght
-int led = 13; //Pin #13
+const int led = 13; //Pin 13
 
-//Headers
-void line(int pTimes);
-void dot(int pTimes);
+char letter; //Stores a char
+String morse; //Stores the translation of a letter to morse code
+
+//Lookup table of the alphabet in Morse code
+String lookupTable[] = {
+  ".-", "-...", "-.-.", "-..", ".", //A-E
+  "..-.", "--.", "....", "..", ".---", //F-J
+  "-.-", ".-..", "--", "-.", //K-N
+  "---", ".--.", "--.-", ".-.", "...", //O-S
+  "-", "..-", "...-", ".--", "-..-", //T-X
+  "-.--", "--.." //Y-Z
+};
+
+//Prototypes
+void line();
+void dot();
 
 void setup() {
-  Serial.begin(9600); //Start connection with Arduino
+  Serial.begin(9600); //Starts connection with Arduino
   pinMode(led, OUTPUT);
 }
+
 void loop() {
-  while(Serial.available()){ //Wait for the message
-    //Store a letter from the message.
-    letter = Serial.read();
-    message.concat(letter);
-    messageLen++;
-  }
-  if (message != "" || !Serial.available()) {
-    delay(500); //Wait before turns the LED on.
-    for(int i = 0; i < messageLen; i++){
-      if(message[i] == 'A'){
-        dot(1);
-        line(1);
-      }
-      if(message[i] == 'B'){
-        line(1);
-        dot(3);
-      }
-      if(message[i] == 'C'){
-        line(1);
-        dot(1);
-        line(1);
-        dot(1);
-      }
-      if(message[i] == 'D'){
-        line(1);
-        dot(2);
-      }
-      if(message[i] == 'E'){
-        dot(1);
-      }
-      if(message[i] == 'F'){
-        dot(2);
-        line(1);
-        dot(1);
-      }
-      if(message[i] == 'G'){
-        line(2);
-        dot(1);
-      }
-      if(message[i] == 'H'){
-        dot(4);
-      }
-      if(message[i] == 'I'){
-        dot(2);
-      }
-      if(message[i] == 'J'){
-        dot(1);
-        line(3);
-      }
-      if(message[i] == 'K'){
-        line(1);
-        dot(1);
-        line(1);
-      }
-      if(message[i] == 'L'){
-        dot(1);
-        line(1);
-        dot(2);
-      }
-      if(message[i] == 'M'){
-        line(2);
-      }
-      if(message[i] == 'N'){
-        line(1);
-        dot(1);
-      }
-      if(message[i] == 'Ñ'){
-        line(2);
-        dot(1);
-        line(2);
-      }
-      if(message[i] == 'O'){
-        line(3);
-      }
-      if(message[i] == 'P'){
-        dot(1);
-        line(2);
-        dot(1);
-      }
-      if(message[i] == 'Q'){
-        line(2);
-        dot(1);
-        line(1);
-      }
-      if(message[i] == 'R'){
-        dot(1);
-        line(1);
-        dot(1);
-      }
-      if(message[i] == 'S'){
-        dot(3);
-      }
-      if(message[i] == 'T'){
-        line(1);
-      }
-      if(message[i] == 'U'){
-        dot(2);
-        line(1);
-      }
-      if(message[i] == 'V'){
-        dot(3);
-        line(1);
-      }
-      if(message[i] == 'W'){
-        dot(1);
-        line(2);
-      }
-      if(message[i] == 'X'){
-        line(1);
-        dot(2);
-        line(1);
-      }
-      if(message[i] == 'Y'){
-        line(1);
-        dot(1);
-        line(2);
-      }
-      if(message[i] == 'Z'){
-        line(2);
-        dot(2);
-      }
-      if(message[i] == ' '){
-        delay(1000);
-      }
-      delay(1000);
+  while(Serial.available()){ //Waits for a word
+    letter = Serial.read(); //Gets a byte from the serial port
+    morse = "";
+    
+    if(64 < letter && letter < 91){ //Letter A to Z
+      letter-=65;
     }
-    //Clean the message
-    message = "";
-    messageLen = 0;
+    else if(96 < letter && letter < 123){ //Letter a to z
+      letter-=97;
+    }
+    
+    if(letter > 0 and letter < 27){
+      morse = lookupTable[letter]; //Gets translation
+    }
+    
+    if(morse.length() > 0){
+      for(int i = 0; i < morse.length(); i++){
+        if(morse[i] == '-'){
+          line();
+        }
+        else{
+          dot();
+        }
+      }
+    }
+    
+    delay(500); //Delay before the next letter
+    
   }
 }
-
-void line(int pTimes){
-  int times = 0;
-  while(times < pTimes){
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(300);
-    times++;
-  }
+//'Flashs' a line
+void line(){
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(300);
 }
 
-void dot(int pTimes){
-  int times = 0;
-  while(times < pTimes){
-    digitalWrite(led, HIGH);
-    delay(300);
-    digitalWrite(led, LOW);
-    delay(300);
-    times++;
-  }
+//'Flashs' a dot
+void dot(){
+  digitalWrite(led, HIGH);
+  delay(300);
+  digitalWrite(led, LOW);
+  delay(300);
 }
